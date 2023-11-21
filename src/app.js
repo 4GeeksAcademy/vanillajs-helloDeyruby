3a5c4bd620780fc3702b616940ea203d9d66f8b4
Dey <128061593+Deyruby@users.noreply.github.com>
2023-11-21 21:49:39 +0000

    tarea terminada definitivamente

--- src/app.js
+++ src/app.js
@@ -9,12 +9,14 @@ window.onload = function() {
   let pronoun = ["the", "our"];
   let adj = ["great", "big", "amazing", "fantastic"];
   let noun = ["jogger", "racoon", "page", "site"];
+  let dom = [".com", ".es", ".cl", ".us"];
 
   for (let i = 0; i < pronoun.length; i++) {
-    git;
     for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++)
-        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dom.length; l++)
+          console.log(pronoun[i] + adj[j] + noun[k] + dom[l]);
+      }
     }
   }
 };
