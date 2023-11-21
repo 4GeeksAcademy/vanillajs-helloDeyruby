11bcb366c26924bb71f8dc0a6c6a772175b61fef
Dey <128061593+Deyruby@users.noreply.github.com>
2023-11-21 17:31:10 +0000

    Tarea terminada

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big", "amazing", "fantastic"];
+  let noun = ["jogger", "racoon", "page", "site"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    git;
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++)
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+    }
+  }
 };
